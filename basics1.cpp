536ecf1f98a3874834a9b32123aad53d80f6fb16
SoumyadeepDey-snax <67054089+SoumyadeepDey-snax@users.noreply.github.com>
2023-03-25 12:54:46 +0530

    Add files via upload

---
+++ basics1.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    cout << "hello"<<" "<<"me\n";
+    cout << sqrt(64) << endl;
+
+    // char, int, float, double, bool
+    int a = 3.5;
+    cout << a << endl; // 3
+
+    bool t = true;
+    bool f = false;
+
+    cout << t << " " << f << endl;
+
+    // any int other that 0 is true only 0 is false
+
+    // long int, long long int
+
+    long long l = 123456789;
+
+    cout << 20%7 << endl;
+
+    // a++ post increment(first use then increment), ++a pre incr
+
+    char c = 'a';
+    // 65-->90 , 97 -->122
+
+    cout << (int)c <<endl;
+
+    //input
+    double z;
+    cin>>z;
+    cout<<z;
+
+    // cin takes input itteratively as encounters space or \n
+
+}
