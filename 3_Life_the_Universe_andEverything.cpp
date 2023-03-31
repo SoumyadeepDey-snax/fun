88f90eaa15b195569df8adb72a9fa5215b99fe3e
SoumyadeepDey-snax <67054089+SoumyadeepDey-snax@users.noreply.github.com>
2023-03-31 11:54:33 +0530

    Add files via upload

---
+++ 3_Life_the_Universe_andEverything.cpp
@@ -0,0 +1,17 @@
+// link - https://www.spoj.com/problems/TEST/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	
+	while(n!=42){
+		cout<<n<<endl;
+		cin>>n;
+	}
+	return 0;
+}
