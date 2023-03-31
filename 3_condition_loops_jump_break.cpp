7a82898317389453d43332d3d8ffa576bde3bf05
SoumyadeepDey-snax <67054089+SoumyadeepDey-snax@users.noreply.github.com>
2023-03-31 11:13:19 +0530

    Add files via upload

---
+++ 3_condition_loops_jump_break.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+int x =5; //global variable scope the whole code
+
+int main() {
+    cout << (2 != 3) << endl;
+
+    // && - true if all true, || - true if any of them true
+    // scope defined in curly brackets {}
+
+    int t =5;
+    while(t--){
+        cout<< t<<" ";
+    }
+    cout<<endl;
+    // 5 4 3 2 1
+
+    // for(initialization; conditions; operations){
+    //}
+
+    for(int i = 1; i<=10; cout << i++ <<endl){
+
+    }
+
+    // infinite loop 
+    // while (1) {}
+
+    //break and continue
+    // break : when it sees break ends the last loop you are in
+    // continue : when it sees continues skips rest content of last loop
+    // for that iteration
+
+    // take out last x digits of a number n, n % 10^x;
+
+
+}
